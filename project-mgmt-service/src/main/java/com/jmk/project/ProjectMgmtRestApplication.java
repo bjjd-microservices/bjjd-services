a78fefa85a89eccd941c200590f0411d39e163cf
rajivbansal2981 <rajivbansal2981@gmail.com>
2020-02-26 20:35:16 +0530

    Latest commit on 26-Feb-2020

---
+++ project-mgmt-service/src/main/java/com/jmk/project/ProjectMgmtRestApplication.java
@@ -0,0 +1,32 @@
+package com.jmk.project;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableEurekaClient
+@EnableJpaRepositories
+@EnableCircuitBreaker
+@ComponentScan(basePackages = { "com.jmk" })
+public class ProjectMgmtRestApplication {
+
+	public static void main(String[] args) throws Exception {
+   	 SpringApplication.run(ProjectMgmtRestApplication.class,args);
+   }
+
+	  @Bean
+	    public ModelMapper modelMapper() {
+	    	return new ModelMapper();
+	    }
+	    
+	
+}
---
+++ user-mgmt-service/src/main/java/com/jmk/user/UserMgmtRestApplication.java
@@ -0,0 +1,34 @@
+package com.jmk.user;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableEurekaClient
+@EnableCircuitBreaker
+@ComponentScan(basePackages ="com.jmk")
+public class UserMgmtRestApplication {
+
+    public static void main(String[] args) throws Exception {
+    	 SpringApplication.run(UserMgmtRestApplication.class,args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+    	return new ModelMapper();
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+}
