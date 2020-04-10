e1d5098b7a36b6f0eab6df67c9c9e60d15e95731
rajivbansal2981 <rajivbansal2981@gmail.com>
2020-04-10 19:58:33 +0530

    Jai Mata Ki

---
+++ data-upload-service/src/main/java/com/jmk/upload/DataUploadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jmk.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataUploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataUploadServiceApplication.class, args);
+	}
+
+}
