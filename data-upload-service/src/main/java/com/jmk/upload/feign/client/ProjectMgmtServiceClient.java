af03b9f03ae1bf3db0685d1210f1216c90d1ea37
rajivbansal2981 <rajivbansal2981@gmail.com>
2020-12-24 17:27:13 +0530

    Jai Mata Ki

--- data-upload-service/src/main/java/com/jmk/upload/feign/client/ProjectMgmtServiceClient.java
+++ data-upload-service/src/main/java/com/jmk/upload/feign/client/ProjectMgmtServiceClient.java
@@ -12,7 +12,7 @@ import com.jmk.project.model.Project;
 @FeignClient(name="project-mgmt-service")
 public interface ProjectMgmtServiceClient {
 
-	@PostMapping(value="/api/project-mgmt-service/project/createWithArray",produces="application/json",consumes="application/json")
+	@PostMapping(value="/api/project-mgmt-service/project/createProjects",produces="application/json",consumes="application/json")
 	ResponseEntity<List<Project>> createProjects(@RequestBody List<Project> projects);
 	
 }
