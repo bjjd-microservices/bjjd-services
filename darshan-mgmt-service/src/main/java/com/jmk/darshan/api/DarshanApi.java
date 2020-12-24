a998f3f66b5c3559f2c48b0613e0a3882c1e89ab
rajivbansal2981 <rajivbansal2981@gmail.com>
2020-12-24 23:49:12 +0530

    JaiMataKi

--- darshan-mgmt-service/src/main/java/com/jmk/darshan/api/DarshanApi.java
+++ darshan-mgmt-service/src/main/java/com/jmk/darshan/api/DarshanApi.java
@@ -5,23 +5,24 @@
  */
 package com.jmk.darshan.api;
 
-import com.jmk.darshan.model.Darshan;
 import java.util.List;
-import io.swagger.annotations.*;
+
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
-import javax.validation.constraints.*;
-import java.util.List;
+import com.jmk.darshan.model.Darshan;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-05-12T00:51:00.325+05:30")
 
 @Api(value = "darshan", description = "the darshan API")
@@ -64,7 +65,7 @@ public interface DarshanApi {
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "Successfully found"),
         @ApiResponse(code = 500, message = "Internal Server Error") })
-    @RequestMapping(value = "/createWithArray",
+    @RequestMapping(value = "/saveDarshans",
         produces = { "application/json" }, 
         consumes = { "application/json" },
         method = RequestMethod.POST)
@@ -81,5 +82,4 @@ public interface DarshanApi {
         consumes = { "application/json" },
         method = RequestMethod.PUT)
     ResponseEntity<Darshan> updateDarshanById(@ApiParam(value = "Darshan Id",required=true) @PathVariable("id") Long id,@ApiParam(value = "" ,required=true )  @Valid @RequestBody Darshan body);
-
 }
