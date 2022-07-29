9852c218ca0e65a5d3a70ccc1585050d0e41606d
rajivbansal2981 <rajivbansal2981@DESKTOP-M89N8PJ>
2022-07-29 17:48:38 +0530

    JaiMataKi

--- cloud-gateway-service/src/main/java/com/jmk/cloud/gateway/config/RouteLocatorConfiguration.java
+++ cloud-gateway-service/src/main/java/com/jmk/cloud/gateway/config/RouteLocatorConfiguration.java
@@ -1,6 +1,8 @@
 package com.jmk.cloud.gateway.config;
 
+import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
@@ -10,6 +12,9 @@ import org.springframework.context.annotation.Configuration;
 @SuppressWarnings("deprecation")
 @Configuration
 public class RouteLocatorConfiguration {
+	
+	@Resource(name="requestHeaderFilter")
+	private GatewayFilter gatewayFilter;
 	
 	@Autowired
 	private TokenRelayGatewayFilterFactory filterFactory;
@@ -24,10 +29,30 @@ public class RouteLocatorConfiguration {
 				.id("user-service"))
 
 				.route(r -> r.path("/api/project-service/project/**")
-				.filters(f -> f.filter(filterFactory.apply())
-				.removeRequestHeader("Cookie"))
-				.uri("lb://project-service/")//.uri("http://127.0.0.1:5379")
-				.id("project-service"))
+						.filters(f -> f.filter(gatewayFilter))
+						.uri("lb://project-service/")
+						.id("project-service"))
+				
+				.route(r -> r.path("/api/people-service/**")
+						.filters(f -> f.filter(gatewayFilter))
+						.uri("lb://people-service/")
+						.id("people-service"))
+				
+				.route(r -> r.path("/api/darshan-service/**")
+						.filters(f -> f.filter(gatewayFilter))
+						.uri("lb://darshan-service/")
+						.id("darshan-service"))
+				
+				.route(r -> r.path("/api/account-service/**")
+						.filters(f -> f.filter(gatewayFilter))
+						.uri("lb://account-service/")
+						.id("account-service"))
+				
+				.route(r -> r.path("/api/data-upload-service/**")
+						.filters(f -> f.filter(gatewayFilter))
+						.uri("lb://data-upload-service/")
+						.id("data-upload-service"))
+		
 				.build();
 	}
 		
