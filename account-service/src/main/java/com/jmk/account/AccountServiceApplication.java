3f3d1533b70646f2b9808dbe23328e0696f723c6
rajivbansal2981 <rajivbansal2981@DESKTOP-M89N8PJ>
2022-08-01 12:17:33 +0530

    JaiMataKi

--- account-service/src/main/java/com/jmk/account/AccountServiceApplication.java
+++ account-service/src/main/java/com/jmk/account/AccountServiceApplication.java
@@ -26,6 +26,7 @@ public class AccountServiceApplication {
 
     public static void main(String[] args) throws Exception {
         new SpringApplication(AccountServiceApplication.class).run(args);
+        
     }
     
 }
