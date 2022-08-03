e955b11ad54ac30677f8e553518fb875511a7ff6
rajivbansal2981 <rajivbansal2981@gmail.com>
2022-08-03 07:15:53 +0530

    JaiMataKi

--- common-service/src/main/java/com/jmk/logger/CommonLogger.java
+++ common-service/src/main/java/com/jmk/logger/CommonLogger.java
@@ -1,12 +1,13 @@
 package com.jmk.logger;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-import lombok.extern.slf4j.Slf4j;
+
 
 @Aspect
 @Slf4j
