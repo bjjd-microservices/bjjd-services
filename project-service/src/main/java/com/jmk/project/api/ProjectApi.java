3d1831b9a25a6e47f0d3717037b2bf20407d68ae
rajivbansal2981 <rajivbansal2981@gmail.com>
2024-09-03 11:18:58 +0530

    JaiMataKi

--- project-service/src/main/java/com/jmk/project/api/ProjectApi.java
+++ project-service/src/main/java/com/jmk/project/api/ProjectApi.java
@@ -78,10 +78,7 @@ public interface ProjectApi {
         @ApiResponse(code = 400, message = "Invalid Project name and password supplied"),
         @ApiResponse(code = 404, message = "Project not found or inactive"),
         @ApiResponse(code = 500, message = "Internal Server Error") })
-    @RequestMapping(value = "/{id}",
-        produces = { "application/json" }, 
-        consumes = { "application/json" },
-        method = RequestMethod.GET)
+    @GetMapping(value = "/{id}", produces = { "application/json", "application/xml" })
     ResponseEntity<Project> findProjectDetailsById(@ApiParam(value = "Project Id",required=true) @PathVariable("id") Long id);
 
 
